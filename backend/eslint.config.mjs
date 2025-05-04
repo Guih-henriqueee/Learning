daa30164d406518b4368c14230ffc10d480ae420
Guilherme Henrique <ghenrique@inpower-sp.local>
2025-05-04 02:09:16 -0300

    fix: resolve test step failure on CI
    - Refactored jest configuration to support TypeScript and ESM
    - Updated app.test.ts with correct imports and structure
    - Modified app.ts to properly export routes for testing

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,36 +1,38 @@
 /* eslint-disable */
-import eslint  from '@eslint/js'
+import eslint from '@eslint/js'
 
 export default [
-  eslint.configs.recommended,
-  {
-    ignores: [
-        'build/',
-        'dist/',
-        'out/',
-        '*.min.js',
-        '*.bundle.js',
-        '*.map',
-        '.eslintrc.js',
-        'eslint.config.js',
-        '**/backend/__tests__/',
-        'commit-lint.config.js'
-      ],
-    rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single']
+    eslint.configs.recommended,
+    {
+        ignores: [
+            'build/',
+            'dist/',
+            'out/',
+            '*.min.js',
+            '*.bundle.js',
+            '*.map',
+            '.eslintrc.js',
+            'eslint.config.js',
+            '**/backend/__tests__/',
+            'commit-lint.config.js'
+        ],
+        rules: {
+            semi: ['error', 'always'],
+            quotes: ['error', 'single']
+        },
+        env: {
+            node: true,
+        },
+        languageOptions:{
+            globals: {
+                describe: 'readonly',
+                it: 'readonly',
+                expect: 'readonly',
+                exports: 'readonly',
+                require: 'readonly',
+                process: 'readonly',
+                console: 'readonly',
+            },
     },
-    env: {
-        node: true,  
-      },
-      globals: {
-        describe: 'readonly',  
-        it: 'readonly',        
-        expect: 'readonly',    
-        exports: 'readonly',
-        require: 'readonly',
-        process: 'readonly',
-        console: 'readonly',
-      },
   }
 ]
