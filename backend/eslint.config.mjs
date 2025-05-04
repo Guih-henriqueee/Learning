cfb8bf463c05c8e23f3db4d6aa0644daba17b0d5
Guilherme Henrique <ghenrique@inpower-sp.local>
2025-05-04 01:59:10 -0300

    fix: resolve test step failure on CI
    - Refactored jest configuration to support TypeScript and ESM
    - Updated app.test.ts with correct imports and structure
    - Modified app.ts to properly export routes for testing

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -3,6 +3,18 @@ import eslint  from '@eslint/js'
 export default [
   eslint.configs.recommended,
   {
+    ignores: [
+        'build/',
+        'dist/',
+        'out/',
+        '*.min.js',
+        '*.bundle.js',
+        '*.map',
+        '.eslintrc.js',
+        'eslint.config.js',
+        '**/backend/__tests__/',
+        'commit-lint.config.js'
+      ],
     rules: {
       semi: ['error', 'always'],
       quotes: ['error', 'single']
